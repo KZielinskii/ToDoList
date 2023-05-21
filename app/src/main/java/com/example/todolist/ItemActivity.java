04a64ce12d4a8bbcae68ac7966b94926bf2cde51
KZielinskii <58587948+KZielinskii@users.noreply.github.com>
2023-05-21 23:41:31 +0200

    Added new Activity

--- app/src/main/java/com/example/todolist/ItemActivity.java
+++ app/src/main/java/com/example/todolist/ItemActivity.java
@@ -8,10 +8,16 @@ import com.example.todolist.R;
 
 
 public class ItemActivity extends AppCompatActivity {
+    private int position;
+    private String title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
+        position = getIntent().getIntExtra("item_index", -1);
+        title = getIntent().getStringExtra("task_title");
+        
     }
 }
